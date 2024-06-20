457e3b0ab8ea3130b65656f449291346096f663b
Kuba Ślawski <kubaslawski.it@gmail.com>
2024-06-20 10:08:52 +0200

    Prod db + req. file

--- SD/settings.py
+++ SD/settings.py
@@ -46,9 +46,13 @@ elif ENVIRONMENT == 'production':
     DEBUG = False
     ALLOWED_HOSTS = []
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+           'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_URL'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
         }
     }
 
